9691801815982b54e41898c5c56fcf40b0c0e922
zhilin.gao <gaoolin@gmail.com>
2023-10-13 09:30:17 +0800

    新增comparison-api项目

--- comparison-api/src/main/java/com/qtech/comparison/config/MybatisPlusConfig.java
+++ comparison-api/src/main/java/com/qtech/comparison/config/MybatisPlusConfig.java
@@ -23,7 +23,7 @@ public class MybatisPlusConfig {
 	@Bean
 	public MybatisPlusInterceptor mybatisPlusInterceptor() {
 		MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-		interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.IMPALA));
+		interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.POSTGRE_SQL));
 		return interceptor;
 	}
 }
