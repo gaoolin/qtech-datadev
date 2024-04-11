f5510167ecb1ba5263c565dc6a53d102236e7fe3
zhilin.gao <gaoolin@gmail.com>
2024-04-11 11:22:40 +0800

    feat(rabbitmq):
    创建rabbitmq api
    创建打线图比对结果生产和消费接口
    消费后写入doris

---
+++ rabbitmq-api/src/main/java/com/qtech/rabbitmq/App.java
@@ -0,0 +1,17 @@
+package com.qtech.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * author :  gaozhilin
+ * email  :  gaoolin@gmail.com
+ * date   :  2024/04/10 14:42:47
+ * desc   :  SpringBoot启动类
+ */
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args); }
+}
