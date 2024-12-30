d24c5c2edaa56951c3fe3c930f1da1e648e13b30
zhilin.gao <gaoolin@gmail.com>
2024-12-30 14:31:57 +0800

    build(mq-svc): 升级 im-datadev-shared 依赖至 1.0.12 版本
    - 将 im-datadev-shared 依赖从1.0.9 版本升级到 1.0.12 版本
    - 在 AaListParamsParsed 类中添加 @JsonIgnoreProperties 注解以忽略未知属性
    - 移除了 AaListParamsParsed 类中的 prodType 字段

--- mq-svc/src/main/java/com/qtech/mq/domain/AaListParamsParsed.java
+++ mq-svc/src/main/java/com/qtech/mq/domain/AaListParamsParsed.java
@@ -2,6 +2,7 @@ package com.qtech.mq.domain;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.qtech.share.aa.pojo.ImAaListParams;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -22,10 +23,10 @@ import java.util.Date;
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
 @Accessors(chain = true)  // 注解用于启用链式调用风格，这意味着在调用 setter 方法时，可以返回当前对象，从而使得多个 setter 方法可以链式调用。
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AaListParamsParsed extends ImAaListParams {
 
     private String simId;
-    private String prodType;
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date receivedTime;
